d09c4432d0f4e5950ef265f69caf45568c723225
reza mirzaei <rezamirzaeidev@gmail.com>
2022-08-07 15:40:39 +0430

    creat menuburger

--- codes/index.js
+++ codes/index.js
@@ -1,2 +1,26 @@
 let $ = document
 
+const iconMenu = $.querySelector('.icon-menu')
+const containerNav = $.querySelector('.container-nav')
+let flag = true
+
+iconMenu.onclick = ()=>{
+    
+
+    if (flag) {
+        containerNav.style.left = '0px'
+
+        flag = false
+        
+    }else{
+        containerNav.style.left = '-100%'
+        flag = true
+    }
+    
+}
+
+const btnCloseNav = $.querySelector('.icon-close')
+
+btnCloseNav.onclick = ()=>{
+    containerNav.style.left = `-100%`
+}
