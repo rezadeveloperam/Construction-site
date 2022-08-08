a0c55997c625568eee1660c76f0233034c8f859d
reza mirzaei <rezamirzaeidev@gmail.com>
2022-08-08 14:49:56 +0430

    add new sections bar

--- codes/index.js
+++ codes/index.js
@@ -24,3 +24,33 @@ const btnCloseNav = $.querySelector('.icon-close')
 btnCloseNav.onclick = ()=>{
     containerNav.style.left = `-100%`
 }
+
+const counters = $.querySelectorAll('.num')
+const sectionCardContainer = $.querySelector('.sec4-container')
+
+let startCounter = false
+
+window.onscroll = (e)=>{
+
+    if (window.scrollY >= sectionCardContainer.offsetTop) { 
+        if (!startCounter) {
+            counters.forEach(counter => setCounter(counter))
+            
+        }
+        startCounter = true
+    }
+}
+
+function setCounter(el){
+    let elemNumberCount = el.dataset.count 
+
+  let countInterVal =   setInterval(()=>{
+
+      if (el.textContent == elemNumberCount) {
+        clearInterval(countInterVal)
+      }
+               
+        el.textContent++
+    },10)
+
+}
